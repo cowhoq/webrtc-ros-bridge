5b37bae5576c96b47151c41a55f5735c764df32b
szl <1872456455@qq.com>
2024-11-27 16:52:54 +0800

    normative test module

---
+++ test/config/config_test.go
@@ -0,0 +1,60 @@
+package config_test
+
+import (
+	"testing"
+	// 假设你的 config 包在项目根目录下的 "config" 文件夹
+	"github.com/3DRX/webrtc-ros-bridge/config"
+)
+
+func TestCheckfunc(t *testing.T) {
+	tests := []struct {
+		name     string
+		cfg      *config.Config
+		expected bool
+	}{
+		{
+			name: "valid config",
+			cfg: &config.Config{
+				Mode: "receiver",
+				Addr: "10.3.9.3:8080",
+				Topics: []config.TopicConfig{
+					{
+						NameIn:  "image_raw",
+						NameOut: "image",
+						Type:    "sensor_msgs/msg/Image",
+					},
+					{
+						NameIn:  "image_raw",
+						NameOut: "image",
+						Type:    "sensor_msgs/msg/Image",
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "invalid config",
+			cfg: &config.Config{
+				Mode: "",
+				Addr: "",
+				Topics: []config.TopicConfig{
+					{
+						NameIn:  "image_raw",
+						NameOut: "image",
+						Type:    "sensor_msgs/msg/Image",
+					},
+				},
+			},
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			msg := config.CheckCfg(tt.cfg)
+			if msg.Status != tt.expected {
+				t.Errorf("TestCheckfunc failed: expected status %v, got %v", tt.expected, msg.Status)
+			}
+		})
+	}
+}
